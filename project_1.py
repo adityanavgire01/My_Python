ff39e4dccc384132d82e36c8ba8cadb9148bef9c
Aditya <aditya.p.navgire@gmail.com>
2023-11-11 22:31:40 +0530

    project_1.py added . It's YT videos download code

---
+++ project_1.py
@@ -0,0 +1,18 @@
+# import yt_dlp
+
+# #Enter the url for the download
+
+# url = input("Enter video url : ")
+
+# ydl_opts ={}
+
+# with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+#     ydl.download([url])
+
+# print("Video downloaded succesfully!!")
+
+
+#*******************
+# In terminal ->
+# yt_dlp url_address
+#After executing above command the video will get downloaded in present working directory
